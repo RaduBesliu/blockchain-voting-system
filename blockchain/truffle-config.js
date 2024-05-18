538629ac17c0308cc8faca41b0e971b1a4da0f15
Radu Besliu <radu.besliu@atta.systems>
2024-05-18 22:31:55 +0300

    Remove unused file

--- blockchain/truffle-config.js
+++ blockchain/truffle-config.js
@@ -1,36 +1,32 @@
-const HDWalletProvider = require('@truffle/hdwallet-provider');
-require('dotenv').config();
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+require("dotenv").config();
 
-const mnemonic = process.env.MNEMONIC
-const sepoliaEndpoint = process.env.INFURA_URL
+const mnemonic = process.env.MNEMONIC;
+const sepoliaEndpoint = process.env.INFURA_URL;
 
 module.exports = {
   networks: {
     sepolia: {
       provider: () => new HDWalletProvider(mnemonic, sepoliaEndpoint),
-      network_id: 11155111, // Sepolia's network ID
-      gas: 4000000, // Adjust the gas limit as per your requirements
-      confirmations: 2, // Set the number of confirmations needed for a transaction
-      timeoutBlocks: 200, // Set the timeout for transactions
+      network_id: 11155111,
+      gas: 4000000,
+      confirmations: 2,
+      timeoutBlocks: 200,
       networkCheckTimeout: 10000,
-      skipDryRun: true // Skip the dry run option
+      skipDryRun: true,
     },
     development: {
       host: "127.0.0.1",
-      port: 7545, // Ensure this matches Ganache's port
-      network_id: "*" // Match any network id
-    }
+      port: 7545,
+      network_id: "*",
+    },
   },
 
-  // Set default mocha options here, use special reporters, etc.
-  mocha: {
-    // timeout: 100000
-  },
+  mocha: {},
 
-  // Configure your compilers
   compilers: {
     solc: {
-      version: "0.8.13"      // Fetch exact version from solc-bin (default: truffle's version)
-    }
-  }
+      version: "0.8.13",
+    },
+  },
 };
